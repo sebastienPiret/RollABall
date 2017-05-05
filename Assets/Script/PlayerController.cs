b561e0e8f9858a413a73a0abc51c84df509e0ac1
Touten3D-PC\Touten3D <sebpiret@hotmail.com>
2017-05-05 23:14:10 +0200

    constraint on camera Create Player and camera controller Create prefab

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
 }
