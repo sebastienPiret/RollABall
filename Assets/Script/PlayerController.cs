1833cb0ba75df05bdb31474a593e1645353400f3
Touten3D-PC\Touten3D <sebpiret@hotmail.com>
2017-05-06 12:22:45 +0200

    Collect item - Display result

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -21,5 +27,25 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "count: " + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "You win bia gamin!";
+        }
+
     }
 }
